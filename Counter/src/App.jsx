854e4bc49cfd0dddc98613ac2b8bc1e01c806d1d
Kevin0403 <kevinsangani99247@gmail.com>
2023-12-17 00:47:15 +0530

    Counter is implemented

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -4,30 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const addCounter = () => {
+    if(counter <20){
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeCounter = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Counter is {counter}</h2>
+      <button onClick={addCounter}>Add Counter</button>
+      <button onClick={removeCounter}>Remove</button>
     </>
   )
 }
